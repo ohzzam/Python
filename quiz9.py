edc72ee9b0e78ae034b59555047465b2051a094a
ohzzam <ohzzam@gmail.com>
2026-01-11 21:45:52 +0900

    두번째

--- quiz9.py
+++ quiz9.py
@@ -8,19 +8,37 @@
 #         치킨 소진 시 사용자 정리 에러[SoldOutError]을 발생시키고 프로그램 종료
 #         출력 메시지 : "재고가 소진되어 더 이상 주문을 받지 않습니다."
 
-
 chicken = 10
 waiting = 1
 
+class SoldOutError(Exception):
+     def __init__(self,msg):
+           self.msg = msg
+
+     def __str__(msg):      
+           return self.msg
+        
+
 while(True):
     print("[남은 치킨 : {0}]".format(chicken))
-    order = int(input("치킨 몇 마리 주문하시겠습니까?"))
-    if order > chicken:
-        print("재료가 부족합니다.")
-else:
-    print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다. ".format(waiting,order))
-    waiting += 1
-    chicken -= order
-
-
-    
+    try :
+        order = int(input("치킨 몇 마리 주문하시겠습니까?"))        
+        if order < 1:
+            print("잘못된 값을 입력하셨습니다.")
+        if order > chicken or order > 10:
+            print("재료가 부족합니다.")                    
+            
+        
+        print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다. ".format(waiting,order))
+        waiting += 1
+        chicken -= order    
+        if chicken <= 0:
+            raise SoldOutError("재료가 소진되었다.")               
+    except SoldOutError:
+            print("재료가 소진되어 더 이상 주문을 받지 않습니다.")              
+            break
+    except ValueError:
+            print(" 잘못된 값을 입력하셨습니다.")
+            break
+    finally:
+            print("감사합니다.")                  
